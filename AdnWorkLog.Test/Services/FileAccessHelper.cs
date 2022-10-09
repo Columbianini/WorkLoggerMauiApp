e6dc5eca1b01a6ec89347b5c3d5f56a06bb30a46
Tony An <13816554795@163.com>
2022-10-09 23:18:59 +0800

    Attempt to do XUnit Test on Dotnet Maui but fail to test Maui related function

---
+++ AdnWorkLog.Test/Services/FileAccessHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdnWorkLog.Services;
+
+namespace AdnWorkLog.Test.Services
+{
+    public class FileAccessHelper_CheckWhereIsDatabasePath
+    {
+        [Fact(Skip = "https://github.com/jamesmontemagno/DeviceInfoPlugin/issues/20")]
+        public void GetLocalFilePath_InputHaha_ReturnIsNotNull()
+        {
+            string dbPath = FileAccessHelper.GetLocalFilePath("Haha");
+            Assert.NotNull(dbPath);
+        }
+    }
+}
